237f0879a413eccf3f173f77533549a5ec4672b3
laura <ls4824218@gmail.com>
2024-05-24 15:43:19 -0300

    Add files via upload

---
+++ exercicios_aula12.py
@@ -0,0 +1,28 @@
+# exercicios
+# 01
+# with open('aula13.py', 'w') as arq:
+#          os.mkdir('aula13.py')
+
+import shutil
+import os
+
+# 02
+# with os.scandir('C:/Users/aluno/Desktop/aula11_12/') as haha:
+#     for aula13 in haha:
+#         print('arquivo encontrado', aula13)
+
+# 03
+# os.rename('aula13.test', 'aula13_test.py')
+
+# 04 ??????
+# shutil.rmtree('C:/Users/aluno/Desktop/aula11_12/hahahaah')
+
+
+# 05
+# with os.scandir('C:/Users/aluno/Desktop/aula11_12/hahahaa') as hahahaa:
+#      for haha1 in hahahaa:
+#         if haha1.is_file():
+#             print('arquivo encontrado', haha1)
+
+# 06
+#shutil.copytree('C:/Users/aluno/Desktop/aula11_12/hahahaa2', 'hahahaa')
